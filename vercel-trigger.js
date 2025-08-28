7e87bdc9af90879936e045b0175114b6ae8ebe00
Volodymyr Turchak <turchakv@mac.home>
2025-08-28 18:08:47 +0200

    🚀 Триггер деплоя - исправления проблемы с белым экраном

--- vercel-trigger.js
+++ vercel-trigger.js
@@ -1,3 +1,5 @@
 // Vercel deployment trigger
 // Generated at: 2024-12-19 20:30
+// Updated at: 2024-12-19 21:45 - White screen fix deployed
 console.log('Footer v2.0 - height and spacing adjusted');
+console.log('White screen issue fixed - automatic recovery system deployed');
